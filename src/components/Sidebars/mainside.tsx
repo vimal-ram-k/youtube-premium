e21a35db1cf789d05c60c34f48cadfb603281b6b
vimalramk <vimalramk99@gmail.com>
2024-07-25 22:51:27 +0530

    Fixed : Sidebar menu side and visibility issue

--- src/components/Sidebars/mainside.tsx
+++ src/components/Sidebars/mainside.tsx
@@ -12,7 +12,7 @@ function Mainside(props: item) {
       <li className="">
         <Link
           to="/"
-          className=" text-decoration-none text-black  btn  px-5 py-2 list-group-item d-flex align-items-center column-gap-lg-4  rounded-3  "
+          className=" text-decoration-none text-black btn  py-2 d-flex align-items-center justify-content-lg-start justify-content-center column-gap-lg-4 rounded-3  "
         >
           <i className={`bi  ${props.img}`} style={{ fontSize: "1.2rem" }}></i>
           <h1 className=" fw-light fs-6 m-0 d-none d-lg-block">{props.name}</h1>
