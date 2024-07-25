666fea9d959b1bfaf8d03012c7777db6c3a58c60
vimalramk <vimalramk99@gmail.com>
2024-07-25 22:51:27 +0530

    Fixed : Sidebar menu side and visibility issue

--- src/components/Sidebars/sidebars.tsx
+++ src/components/Sidebars/sidebars.tsx
@@ -1,9 +1,10 @@
 import Mainside from "./mainside";
 import data from "./sidebar-data.json";
+import "./sidebar.css";
 
 function Sidebars() {
   return (
-    <div className=" d-none d-md-inline col-2 mt-3 border-bottom py-2 col-lg-3 col-xl-2">
+    <div className="custom-side-width d-none d-md-inline mt-3 border-bottom py-2 ">
       <ul className=" p-0 list-group-flush ">
         {data.sidebarpages.map((items) => {
           return <Mainside img={items.img} name={items.name} />;
