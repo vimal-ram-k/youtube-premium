10f53a0e7121b305a25ba2a6ac34d0e8cf9317a0
vimalramk <vimalramk99@gmail.com>
2024-07-27 11:50:05 +0530

    Fixed : Search mic issue

--- src/components/Searchbar/mainsearchbar.tsx
+++ src/components/Searchbar/mainsearchbar.tsx
@@ -1,7 +1,7 @@
 function Mainsearchbar() {
   return (
-    <>
-      <div className=" border d-none d-md-flex justify-content-between align-items-center col-7 rounded-5  ps-3 py-0">
+    <div className=" d-flex col-7 justify-content-center align-items-center column-gap-4">
+      <div className=" border d-none d-md-flex justify-content-between col-8 align-items-center rounded-5  ps-3 py-0">
         <div className=" d-flex align-items-center column-gap-2" id="searchbar">
           <input
             type="text"
@@ -11,11 +11,14 @@ function Mainsearchbar() {
             className=" border-0 h-100"
           />
         </div>
-        <button className=" btn bg-body-secondary rounded-end-5 px-4 border border-1">
+        <button className=" btn bg-secondary-subtle rounded-end-5 px-4 border border-1">
           <i className="  bi  bi-search"></i>
         </button>
       </div>
-    </>
+      <button className=" btn rounded-5 bg-secondary-subtle">
+        <i className=" bi  bi-mic-fill"></i>
+      </button>
+    </div>
   );
 }
 
